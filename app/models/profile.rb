dda023554ce0a19000802273f27ea4fce4ed665b
C Haden <chaden@spoonflower.com>
2015-03-04 16:50:04 -0500

    gonna commit, because that seems like a good thing to do

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,4 @@
-class Profile
+class Profile < ActiveRecord::Base
 
   def initialize(username)
     @content = HTTParty.get(
@@ -7,6 +7,31 @@ class Profile
                      "User-Agent" => "anyone"
                     }
     )
+  end
+
+  def self.create_from_username( username )
+    response = HTTParty.get(
+      "https://api.github.com/users/#{username}",
+      :headers => {"Authorization" => "token #{ENV['GITHUB_TOKEN']}",
+                   "User-Agent" => "anyone"
+                  }
+      )
+
+    @content = response
+
+    if response["login"]
+      Profile.create(username: response["login"],
+        avatar_url: response["avator_url"],
+        location: response["location"],
+        company_name: response["company"],
+        number_of_followers: response["followers"],
+        number_following: response["following"],
+        repository_list: RepositoryList.new( username )
+        )
+
+    else
+      return nil
+    end
   end
 
   def username
---
+++ db/migrate/20150304184905_make_a_table.rb
@@ -0,0 +1,13 @@
+class MakeATable < ActiveRecord::Migration
+  def change
+    create_table :profiles do |t|
+      t.column :username, :string
+      t.column :avatar_url, :string
+      t.column :location, :string
+      t.column :company_name, :string
+      t.column :number_of_followers, :string
+      t.column :number_following, :string
+      t.column :repository_list, :text
+    end
+  end
+end
