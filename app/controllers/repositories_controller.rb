bb59ba1e9cfcb5eb5e6e910c292f772f7f03c7b5
C Haden <chaden@spoonflower.com>
2015-03-04 20:18:50 -0500

    I am a crackerjack debugger of other people's code. The shoemaker's children go barefoot.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -6,19 +6,6 @@ class RepositoriesController < ApplicationController
   def show
     @profile = Profile.find_by_username(params[:username]) ||
         Profile.create_from_username(params[:username])
-    # @repositories = RepositoryList.create_from_profile_id(params[:username], @profile.id)
-
-        # @repositories = ObjectSpace._id2ref(@profile.repository_list.to_i)
-    # @repositories = RepositoryList.new(params[:username]).repositories
   end
-
-  # def show
-  #   @profile = Profile.new(params[:username])
-  #   if @profile.username.blank?
-  #     redirect_to root_path
-  #   else
-  #     @repositories = RepositoryList.new(params[:username]).repositories
-  #   end
-  # end
 
 end
--- app/models/repository_list.rb
+++ app/models/repository_list.rb
@@ -1,15 +1,4 @@
 class RepositoryList
-  # attr_reader :repositories
-  #
-  # def initialize(username)
-  #   results = HTTParty.get(
-  #       "https://api.github.com/users/#{username}/repos?sort=updated",
-  #       :headers => {"Authorization" => "token #{ENV['GITHUB_TOKEN']}",
-  #                    "User-Agent" => "anyone"
-  #                   }
-  #   )
-  #   @repositories = results.map {|r| Repository.new(r)}
-  # end
 
   def self.create_from_profile_id( username, id )
     results = HTTParty.get(
